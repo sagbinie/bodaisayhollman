abe15bc9f71797c1f9e6498ffb22e1f5fec0c96d
Alejandro G. Sagbini <54915667+sagbinie@users.noreply.github.com>
2024-09-05 21:39:17 -0500

    feat:sparkles:: add wedding page + static files

---
+++ static/js/es-CO.js
@@ -0,0 +1,36 @@
+var lang_nombreRequerido = 'Ingrese nombre';
+var lang_informandoAsistencia = 'Informando asistencia';
+var lang_confirmarAsistencia = 'Enviar';
+
+var lang_caracteresNombreAsistencia = 'El nombre no debe superar 20 caracteres';
+var lang_caracteresComentariosAsistencia =
+  'El mensaje no debe superar 100 caracteres';
+
+var lang_asistenciaMsjExito_1 = 'InformaciÃ³n enviada';
+var lang_asistenciaMsjExito_2 = 'Hemos enviado tu informacion de asistencia';
+
+var lang_cancionRequerida = 'Ingrese nombre de musica / autor';
+var lang_linkIncorrecto = 'Formato link incorrecto';
+var lang_enviandoSugerencia = 'Enviando sugerencia';
+var lang_sugerirCancion = 'Sugerir CanciÃ³n';
+
+var lang_caracteresNombreSugerencia = 'El nombre no debe superar 20 caracteres';
+var lang_caracteresCancionSugerencia =
+  'El titulo/autor no debe superar 50 caracteres';
+var lang_caracteresLinkSugerencia = 'El link no debe superar 250 caracteres';
+
+var lang_sugerirCancionMsjExito_1 = 'Sugerencia enviada';
+var lang_sugerirCancionMsjExito_2 =
+  'Tu sugerencia de canciÃ³n se ha agregado a la lista';
+
+var lang_titleModalAsistenciaCeremonia = 'Â¿Asistes a la ceremonia?';
+var lang_titleModalAsistenciaFiesta = 'Â¿Asistes a la fiesta?';
+
+var lang_titleModalMapaFiesta = 'CÃ³mo llegar a la Fiesta';
+var lang_titleModalMapaCeremonia = 'CÃ³mo llegar a la Ceremonia';
+
+var lang_codigoAccesoVipRequerido = 'Ingrese el cÃ³digo de acceso';
+var lang_btnCodigoVip = 'Acceder';
+var lang_accediendoCodigoVip = 'Accediendo';
+
+var lang_textoFinalCuentaRegresiva = 'Â¡LLEGO EL <br> GRAN DÃA!';
