fdc7069f4d6a2864d89da2ba3861ad51059c2ad9
Alejandro G. Sagbini <54915667+sagbinie@users.noreply.github.com>
2024-09-22 18:28:51 -0500

    feat:sparkles:: add images + dress code

--- static/js/es-CO.js
+++ static/js/es-CO.js
@@ -6,13 +6,13 @@ var lang_caracteresNombreAsistencia = 'El nombre no debe superar 20 caracteres';
 var lang_caracteresComentariosAsistencia =
   'El mensaje no debe superar 100 caracteres';
 
-var lang_asistenciaMsjExito_1 = 'InformaciÃ³n enviada';
+var lang_asistenciaMsjExito_1 = 'Información enviada';
 var lang_asistenciaMsjExito_2 = 'Hemos enviado tu informacion de asistencia';
 
 var lang_cancionRequerida = 'Ingrese nombre de musica / autor';
 var lang_linkIncorrecto = 'Formato link incorrecto';
 var lang_enviandoSugerencia = 'Enviando sugerencia';
-var lang_sugerirCancion = 'Sugerir CanciÃ³n';
+var lang_sugerirCancion = 'Sugerir Canción';
 
 var lang_caracteresNombreSugerencia = 'El nombre no debe superar 20 caracteres';
 var lang_caracteresCancionSugerencia =
@@ -21,15 +21,15 @@ var lang_caracteresLinkSugerencia = 'El link no debe superar 250 caracteres';
 
 var lang_sugerirCancionMsjExito_1 = 'Sugerencia enviada';
 var lang_sugerirCancionMsjExito_2 =
-  'Tu sugerencia de canciÃ³n se ha agregado a la lista';
+  'Tu sugerencia de canción se ha agregado a la lista';
 
-var lang_titleModalAsistenciaCeremonia = 'Â¿Asistes a la ceremonia?';
-var lang_titleModalAsistenciaFiesta = 'Â¿Asistes a la fiesta?';
+var lang_titleModalAsistenciaCeremonia = '¿Asistes a la ceremonia?';
+var lang_titleModalAsistenciaFiesta = '¿Asistes a la fiesta?';
 
-var lang_titleModalMapaFiesta = 'CÃ³mo llegar a la Fiesta';
-var lang_titleModalMapaCeremonia = 'CÃ³mo llegar a la Ceremonia';
+var lang_titleModalMapaFiesta = 'Cómo llegar a la Fiesta';
+var lang_titleModalMapaCeremonia = 'Cómo llegar a la Ceremonia';
 
-var lang_codigoAccesoVipRequerido = 'Ingrese el cÃ³digo de acceso';
+var lang_codigoAccesoVipRequerido = 'Ingrese el código de acceso';
 var lang_btnCodigoVip = 'Acceder';
 var lang_accediendoCodigoVip = 'Accediendo';
 
